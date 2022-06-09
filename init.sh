edd20f1fe6d1d02a9c88a20ba8f780841823a1cf
2lu3 <hi2lu3@gmail.com>
2022-06-09 13:58:33 +0900

    mkdir -p .local/を追加

--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@ set -xe
 # bash -c "$(curl -fsLS https://raw.githubusercontent.com/2lu3/dotfiles/main/init.sh)"
 
 rm ~/.local/share/chezmoi -rf
+mkdir -p ~/.local/
 
 pushd ~/.local/
 sh -c "$(curl -fsLS chezmoi.io/get)" -- init --apply 2lu3

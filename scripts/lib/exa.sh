e4638ac37dcb25ba8ffcacb127700039f2316c8b
2lu3 <hi2lu3@gmail.com>
2022-06-09 12:12:55 +0900

    exaのunzipで-o(overwrite)するようにした

--- scripts/lib/exa.sh
+++ scripts/lib/exa.sh
@@ -2,5 +2,5 @@
 set -xe
 
 wget https://github.com/ogham/exa/releases/download/v0.10.1/exa-linux-x86_64-v0.10.1.zip -O /tmp/exa.zip
-unzip /tmp/exa.zip -d /tmp/
+unzip -o /tmp/exa.zip -d /tmp/
 mv /tmp/bin/exa ~/.local/bin/

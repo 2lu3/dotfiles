332a96cb0d9d570e951f3cd0486c968558dbebc0
2lu3 <hi2lu3@gmail.com>
2022-06-09 11:34:59 +0900

    initに~/.local/share/chezmoiを消すコードを付け加えた

--- init.sh
+++ init.sh
@@ -3,6 +3,8 @@ set -xe
 
 # bash -c "$(curl -fsLS https://raw.githubusercontent.com/2lu3/dotfiles/main/init.sh)"
 
+rm ~/.local/share/chezmoi -rf
+
 pushd ~/.local/
 sh -c "$(curl -fsLS chezmoi.io/get)" -- init --apply 2lu3
 popd

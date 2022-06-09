47a44c5620fce0749e7955bf5c499d42ef8d47f4
2lu3 <hi2lu3@gmail.com>
2022-06-09 11:51:11 +0900

    germaniumのpathが間違っていたので修正

--- scripts/lib/germanium.sh
+++ scripts/lib/germanium.sh
@@ -2,5 +2,5 @@
 set -xe
 
 wget https://github.com/matsuyoshi30/germanium/releases/download/v1.2.2/germanium_1.2.2_linux_x86_64.tar.gz -O /tmp/germanium.tar.gz
-tar -xvf /tmp/germanium_1.2.2_linux_x86_64.tar.gz -C /tmp/
+tar -xvf /tmp/germanium.tar.gz -C /tmp/
 mv /tmp/germanium ~/.local/bin

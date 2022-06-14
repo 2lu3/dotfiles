a48503fa41c812227113cfd20e031380d8511cf3
2lu3 <hi2lu3@gmail.com>
2022-06-14 13:27:40 +0900

    fzfを修正

--- scripts/fzf.sh
+++ scripts/fzf.sh
@@ -6,9 +6,5 @@ set -xe
 if [[ ! -e "$HOME/.fzf" ]]; then
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 
-    pushd ~/.fzf
-    git checkout 0.30.0
-    popd
-
     ~/.fzf/install --no-completion --key-bindings --no-update-rc
 fi

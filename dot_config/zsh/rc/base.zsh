3f1670c4c63e9b9a2c796731de1c2fefc8e0772d
2lu3 <hi2lu3@gmail.com>
2022-06-16 20:06:35 +0900

    なにかうまくいかない

--- dot_config/zsh/rc/base.zsh
+++ dot_config/zsh/rc/base.zsh
@@ -8,3 +8,10 @@ HISTFILE="${Z_DATA_DIR}/zsh_history"    # 履歴を保存するファイル
 HISTSIZE=1000                           # メモリ内の履歴の数
 SAVEHIST=100000                         # 保存される履歴の数
 HISTORY_IGNORE="(ls|l|s|la|ll|cd|pwd|zsh|exit|cd *)"
+
+#--------------------------------------------------------------
+#               Uncategorized
+#--------------------------------------------------------------
+
+
+autoload -Uz compinit; compinit

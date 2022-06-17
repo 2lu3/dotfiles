038696573cbc1ba53bee261af75fabcf7cf68597
2lu3 <hi2lu3@gmail.com>
2022-06-17 23:52:37 +0900

    エイリアスの設定

--- scripts/llvm.sh
+++ scripts/llvm.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -xe
 
-wget https://apt.llvm.org/llvm.sh -O /tmp/llvm.sh
-chmod +x /tmp/llvm.sh
-sudo /tmp/llvm.sh 14 all
+if ! type clang-14 > /dev/null 2>&1; then
+    wget https://apt.llvm.org/llvm.sh -O /tmp/llvm.sh
+    chmod +x /tmp/llvm.sh
+    sudo /tmp/llvm.sh 14 all
+
+    sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-14 14
+    sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-14 14
+    sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-14 14
+fi

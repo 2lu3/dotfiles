785a7eb20a8606c7b39146ba936a9d28dd968ecc
2lu3 <hi2lu3@gmail.com>
2022-06-18 21:36:48 +0900

    Update docker-cli.sh

--- scripts/docker-cli.sh
+++ scripts/docker-cli.sh
@@ -21,8 +21,9 @@ if ! type docker > /dev/null 2>&1; then
     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu\
     focal stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   else
+    echo "OS: ubuntu"
     echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   fi
   

f2cc41142c3a200f54d9014bfa8f617dd781115c
2lu3 <hi2lu3@gmail.com>
2022-06-18 13:42:50 +0900

    appimagelauncherが毎回インストールされていたので

--- scripts/appimagelauncher.sh
+++ scripts/appimagelauncher.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -xe
 
-if ! type appimagelauncher > /dev/null 2>&1; then
+if ! type appimagelauncherd > /dev/null 2>&1; then
     sudo add-apt-repository -y ppa:appimagelauncher-team/stable
     sudo apt-get update
     sudo apt-get install -y appimagelauncher

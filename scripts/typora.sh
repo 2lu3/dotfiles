b9b1a7525e8622330cb48e1b61a2f8d3f9eb87cc
2lu3 <hi2lu3@gmail.com>
2022-06-18 11:13:07 +0900

    typoraとbiscuit

--- scripts/typora.sh
+++ scripts/typora.sh
@@ -4,5 +4,6 @@ set -xe
 if ! type typora > /dev/null 2>&1; then
     wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
     sudo add-apt-repository 'deb https://typora.io/linux ./'
+    sudo apt-get update
     sudo apt-get install -y typora
 fi

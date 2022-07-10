b1b01feeb32a606e107f7d0488c35912a64caa83
2lu3 <hi2lu3@gmail.com>
2022-07-11 07:58:35 +0900

    llvmでapt-get update&upgradeするようにした

--- scripts/llvm.sh
+++ scripts/llvm.sh
@@ -4,6 +4,8 @@ set -xe
 if ! type clang-14 > /dev/null 2>&1; then
     wget https://apt.llvm.org/llvm.sh -O /tmp/llvm.sh
     chmod +x /tmp/llvm.sh
+    sudo apt-get update
+    sudo apt-get upgrade -y
     sudo /tmp/llvm.sh 14 all
 
     sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-14 14

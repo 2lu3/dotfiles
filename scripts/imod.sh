0d602d4ae227aa59673ac238217e0aedb0c12696
2lu3 <hi2lu3@gmail.com>
2022-08-08 15:29:59 +0900

    IMDOを追加した

--- scripts/imod.sh
+++ scripts/imod.sh
@@ -2,4 +2,8 @@
 set -xe
 
 wget https://bio3d.colorado.edu/imod/AMD64-RHEL5/imod_4.11.18_RHEL7-64_CUDA10.1.sh -O /tmp/imod.sh
-sudo sh /tmp/imod.sh -yes
+mkdir -p ~/.local/share/
+mkdir -p ~/.local/bin/
+bash /tmp/imod.sh -yes -dir ~/.local/share -script ~/.local/bin/
+chmod +x ~/.local/bin/IMOD-linux.sh
+

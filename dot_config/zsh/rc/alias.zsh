5ca6ee11ca64bff3a73d955f6e87a222c76b13b5
2lu3 <hi2lu3@gmail.com>
2022-08-22 15:21:42 +0900

    updated

--- dot_config/zsh/rc/alias.zsh
+++ dot_config/zsh/rc/alias.zsh
@@ -1,11 +1,11 @@
 #!/bin/zsh
 
-if [[ $(command -v nvim) ]]; then
+if [[$(command -v nvim)]]; then
     alias v='nvim'
     alias vi='nvim'
 fi
 
-if [[ $(command -v exa) ]]; then
+if [[$(command -v exa)]]; then
     alias l='exa --icons --git --group-directories-first --color=auto'
     alias ls=l
     alias la='ls -a'

529d0bc2f28442148d954116ce5db1467718da92
2lu3 <hi2lu3@gmail.com>
2022-08-23 16:14:05 +0900

    updated

---
+++ dot_config/zsh/rc/prompt.zsh
@@ -0,0 +1,3 @@
+# timeコマンドの見た目を変更する
+# https://dev.classmethod.jp/articles/zsh-time-command-formatting-like-bash/
+TIMEFMT=$'\n\n========================\nProgram : %J\nCPU     : %P\nuser    : %*Us\nsystem  : %*Ss\ntotal   : %*Es\n========================\n'

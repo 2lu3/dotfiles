23654ae43923e3e26907099a392432d662f41407
2lu3 <hi2lu3@gmail.com>
2022-09-02 17:38:34 +0900

    Revert "docker desktopに書き換えた"
    This reverts commit 1a33b0bb5f4b39c0346ec02b8b02fa1f42c1d2a7.

--- scripts/docker-cli.sh
+++ scripts/docker-cli.sh
@@ -26,9 +26,12 @@ if ! type docker > /dev/null 2>&1; then
     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   fi
+  
+  sudo apt-get update
+  
+  sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
-  curl -fsSL "https://desktop.docker.com/linux/main/amd64/docker-desktop-4.11.0-amd64.deb?utm_source=docker&utm_medium=webreferral&utm_campaign=docs-driven-download-linux-amd64" -o /tmp/docker.deb
-  sudo apt-get install  -y /tmp/docker.deb
-  systemctl --user start docker-desktop
-  systemctl --user enable docker-desktop
+  # dockerをsudoなしで実行できるようにする
+  sudo gpasswd -a ${USER} docker
+  sudo systemctl restart docker
 fi

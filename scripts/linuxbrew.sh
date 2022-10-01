92f2bbf94437665023090ccd6b04e4f8b08cceb6
2lu3 <hi2lu3@gmail.com>
2022-10-01 22:23:01 +0900

    linuxbrewが毎回インストールされるバグを修正

--- scripts/linuxbrew.sh
+++ scripts/linuxbrew.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 set -xe
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if [[ "$is_init" = true ]]; then
+    if [[ -e "$HOME/.linuxbrew" ]]; then
+        rm -rf "${HOME}/.linuxbrew"
+    fi
+
+    if [[ -e "/home/linuxbrew" ]]; then
+        sudo rm -rf "/home/linuxbrew"
+    fi
+fi
+
+if [[ ! -e "$HOME/.linuxbrew" ]]; then
+    if [[ ! -e "/home/linuxbrew" ]]; then
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    fi
+fi
 
 test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
 test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"

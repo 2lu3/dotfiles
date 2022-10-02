32e51e1ed94da619f1d302c6d956465179a00bf2
2lu3 <hi2lu3@gmail.com>
2022-10-02 06:39:48 +0000

    updated

--- scripts/docker-cli.sh
+++ scripts/docker-cli.sh
@@ -2,6 +2,10 @@
 set -xe
 
 
+if [[ "$is_init" = true ]]; then
+  sudo apt-get remove docker docker-engine docker.io containerd runc
+fi
+
 
 if ! type docker > /dev/null 2>&1; then
 
@@ -26,9 +30,9 @@ if ! type docker > /dev/null 2>&1; then
     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   fi
-  
+
   sudo apt-get update
-  
+
   sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
   # dockerをsudoなしで実行できるようにする

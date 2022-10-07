2e66a53a28aeb33d03d06e6606eb5c0ca6282727
2lu3 <hi2lu3@gmail.com>
2022-10-07 22:29:57 +0900

    fontを修正

--- scripts/font.sh
+++ scripts/font.sh
@@ -3,6 +3,9 @@ set -xe
 
 #sudo apt-get install -y fontconfig fonts-ipaexfont
 
-mkdir -p ~/.fonts/
+mkdir -p ~/.fonts
 wget https://github.com/mzyy94/RictyDiminished-for-Powerline/raw/master/RictyDiminished-Regular.ttf -O ~/.fonts/RictyDiminished-Regular.ttf
-wget https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraCode/Regular/complete/Fira%20Code%20Regular%20Nerd%20Font%20Complete%20Mono.ttf -O ~/.fonts/FiraCodeNF.ttf
+wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/FiraCode/Regular/complete/Fira%20Code%20Regular%20Nerd%20Font%20Complete%20Mono.ttf -O ~/.fonts/FiraCodeNF.ttf
+
+
+fc-cache -f -v

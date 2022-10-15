b84edcd4f0a567621a9a747b4332305268215596
2lu3 <hi2lu3@gmail.com>
2022-10-15 12:00:35 +0900

    Update gnome.sh

--- scripts/lib/gnome.sh
+++ scripts/lib/gnome.sh
@@ -4,7 +4,7 @@ set -xe
 # https://askubuntu.com/questions/984205/how-to-save-gnome-settings-in-a-file
 
 mkdir -p ~/.backup/gnome
-dconf dump / > ~/.backup/gnome/$(date +%Y%m%d)
+dconf dump / > ~/.backup/gnome/$(date +%Y%m%d-%H%M%S)
 
 pushd $(dirname $0)
 dconf load / < ../data/gnome

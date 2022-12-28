6e9acf1558d6cbf5cd56af8538cf73c81ca7bdd8
2lu3 <hi2lu3@gmail.com>
2022-12-28 17:27:44 +0900

    exaをaptでなくバイナリ

--- scripts/lib/exa.sh
+++ scripts/lib/exa.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 set -xe
 
-sudo apt-get install -y exa
+#!/bin/bash
+set -xe
+
+if [[ "$is_init" = true ]]; then
+    rm ~/.local/bin/exa -f
+fi
+
+if ! type exa > /dev/null 2>&1; then
+    wget https://github.com/ogham/exa/releases/download/v0.10.1/exa-linux-x86_64-v0.10.1.zip -O /tmp/exa.zip
+    unzip -o /tmp/exa.zip -d /tmp/
+    mv /tmp/bin/exa ~/.local/bin/
+fi

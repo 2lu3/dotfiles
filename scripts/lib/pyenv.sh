bf2d3fa1abc8539198e365feb9e5129067cb0ba5
2lu3 <hi2lu3@gmail.com>
2023-03-12 22:28:30 +0900

    updated

--- scripts/lib/pyenv.sh
+++ scripts/lib/pyenv.sh
@@ -8,17 +8,15 @@ sudo apt-get install -y build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev curl llvm \
 libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev libopencv-dev git
 
+# https://stackoverflow.com/questions/74834388/ubuntu-configure-wxwidgets-always-gives-error-the-development-files-for-gtk-we
+sudo apt-get install -y libgtk-3-dev
+
 if [[ "$is_init" = true ]]; then
     rm ~/.pyenv -rf
 fi
 
 if [[ ! -e "$HOME/.pyenv" ]]; then
-    git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-
-    pushd ~/.pyenv
-    src/configure
-    make -C src
-    popd
+    curl https://pyenv.run | bash
 
     export PYENV_ROOT="$HOME/.pyenv"
     command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"

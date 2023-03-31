e4af50079a0caef4d2eed27b0b2cc080c4adfa73
2lu3 <hi2lu3@gmail.com>
2023-03-31 13:04:01 +0900

    fontのインストール先を変えた

--- scripts/lib/font.sh
+++ scripts/lib/font.sh
@@ -4,8 +4,8 @@ set -xe
 #sudo apt-get install -y fontconfig fonts-ipaexfont
 sudo apt-get install -y fontconfig
 
-mkdir -p ~/.fonts
-wget https://github.com/mzyy94/RictyDiminished-for-Powerline/raw/master/RictyDiminished-Regular.ttf -O ~/.fonts/RictyDiminished-Regular.ttf
-wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/FiraCode/Regular/complete/Fira%20Code%20Regular%20Nerd%20Font%20Complete%20Mono.ttf -O ~/.fonts/FiraCodeNF.ttf
+mkdir -p ~/.local/share/fonts
+wget https://github.com/mzyy94/RictyDiminished-for-Powerline/raw/master/RictyDiminished-Regular.ttf -O ~/.local/share/fonts/RictyDiminished-Regular.ttf
+wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/FiraCode/Regular/complete/Fira%20Code%20Regular%20Nerd%20Font%20Complete%20Mono.ttf -O ~/.local/share/fonts/FiraCodeNF.ttf
 
 fc-cache -f -v

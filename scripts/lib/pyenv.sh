385bfe37356331510eaf8dbcd47cfd8a86d73de2
2lu3 <hi2lu3@gmail.com>
2023-04-19 17:02:26 +0900

    tkinterを追加

--- scripts/lib/pyenv.sh
+++ scripts/lib/pyenv.sh
@@ -7,7 +7,7 @@ sudo apt-get update
 sudo apt-get install -y python3 python3-pip python3-venv
 sudo apt-get install -y build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev curl llvm \
-libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev libopencv-dev git
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev libopencv-dev git python3-tk
 
 # https://stackoverflow.com/questions/74834388/ubuntu-configure-wxwidgets-always-gives-error-the-development-files-for-gtk-we
 sudo apt-get install -y libgtk-3-dev

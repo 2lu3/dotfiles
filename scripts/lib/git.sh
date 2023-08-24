ef7e9df8dcd8b46cb162d8943ae3e3b6fdce27ef
2lu3 <hi2lu3@gmail.com>
2023-08-24 10:30:30 +0900

    git lfsを追加

--- scripts/lib/git.sh
+++ scripts/lib/git.sh
@@ -4,3 +4,6 @@ set -xe
 git config --global include.path ~/.config/git/conf.conf
 git config --global user.name ${CONFIG_USER_NAME}
 git config --global user.email ${CONFIG_USER_EMAIL}
+
+curl -fsSL https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+sudo apt-get install -y git-lfs

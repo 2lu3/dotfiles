0b59101527ef4ec271aa8653124f011bc5e14152
2lu3 <hi2lu3@gmail.com>
2023-09-04 23:36:43 +0900

    updated

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,12 +1,12 @@
 require 'format'
 require 'status'
-require 'event'
+--require 'event'
 
 return {
     keys = require("keybinds").keys,
     key_tables = require("keybinds").key_tables,
     leader = { key = "a", mods = "CTRL" },
-    disable_default_key_bindings = false,
+    disable_default_key_bindings = true,
     color_scheme = 'Catppuccin Mocha',
     window_background_opacity = 0.8,
     font = require("wezterm").font("FiraCode Nerd Font Mono"),

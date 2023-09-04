586616b02f8b11b10ac3ea76da3f748a9fbcd2dd
2lu3 <hi2lu3@gmail.com>
2023-09-04 22:41:55 +0900

    weztermを追加

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,6 +1,16 @@
+require 'format'
+require 'status'
+require 'event'
+
 return {
+    keys = require("keybinds").keys,
+    key_tables = require("keybinds").key_tables,
+    leader = { key = "a", mods = "CTRL" },
+    disable_default_key_bindings = false,
     color_scheme = 'Catppuccin Mocha',
-    window_background_opacity = 0.9,
+    window_background_opacity = 0.8,
     font = require("wezterm").font("FiraCode Nerd Font Mono"),
     font_size = 11.0,
+
+    status_update_interval = 1000,
 }

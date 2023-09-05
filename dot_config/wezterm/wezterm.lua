49d78e81454996b97b724ea26425105f9366f9a7
2lu3 <hi2lu3@gmail.com>
2023-09-05 16:52:24 +0900

    灯化率を下げた

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -8,7 +8,7 @@ return {
     leader = { key = "a", mods = "CTRL" },
     disable_default_key_bindings = true,
     color_scheme = 'Catppuccin Mocha',
-    window_background_opacity = 0.9,
+    window_background_opacity = 0.8,
     font = require("wezterm").font("FiraCode Nerd Font Mono"),
     font_size = 11.0,
 

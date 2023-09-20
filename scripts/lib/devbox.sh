0b02e6bac0749f4ddadbd937d001a95d279b2731
2lu3 <hi2lu3@gmail.com>
2023-09-20 09:41:06 +0900

    udpated

--- scripts/lib/devbox.sh
+++ scripts/lib/devbox.sh
@@ -4,16 +4,6 @@ set -xe
 pushd $(dirname $0)
 source ./utils.sh
 
-# nix
-if should_install nix; then
-    curl -L https://nixos.org/nix/install -o /tmp/nix.sh
-    sh /tmp/nix.sh
-fi
 
-# devbox
-if should_install devbox; then
-    curl -fsSL https://get.jetpack.io/devbox -o /tmp/devbox.sh
-    bash /tmp/devbox.sh -f
-fi
 
 popd
--- scripts/lib/pyenv.sh
+++ scripts/lib/pyenv.sh
@@ -3,14 +3,13 @@ set -xe
 
 # https://github.com/pyenv/pyenv#basic-github-checkout
 
-sudo apt-get update
-sudo apt-get install -y python3 python3-pip python3-venv
-sudo apt-get install -y build-essential libssl-dev zlib1g-dev \
+sudo apt-get install -y python3 python3-pip python3-venv \
+build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev curl llvm \
-libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev libopencv-dev git python3-tk
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev libopencv-dev git python3-tk libgtk-3-dev
 
+# libgtk-3-dev
 # https://stackoverflow.com/questions/74834388/ubuntu-configure-wxwidgets-always-gives-error-the-development-files-for-gtk-we
-sudo apt-get install -y libgtk-3-dev
 
 if [[ "$is_init" = true ]]; then
     rm ~/.pyenv -rf

971c2a8c116ec6343ff50ee3159a0b857e467bc0
2lu3 <hi2lu3@gmail.com>
2023-10-08 16:57:14 +0900

    change colorscheme

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -7,7 +7,7 @@ return {
     key_tables = require("keybinds").key_tables,
     leader = { key = "a", mods = "CTRL" },
     disable_default_key_bindings = true,
-    color_scheme = 'Catppuccin Mocha',
+    color_scheme = 'MaterialDesignColors',
     window_background_opacity = 0.9,
     font = require("wezterm").font("FiraCode Nerd Font"),
     font_size = 11.0,

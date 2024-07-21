776de4669d2f1de13ce110bcf766c5d6ea50145a
2lu3 <hi2lu3@gmail.com>
2024-07-22 01:33:12 +0900

    flatpak updated

--- scripts/gui.sh
+++ scripts/gui.sh
@@ -8,7 +8,7 @@ source lib/utils.sh
 #------------------------------------
 # flatpak
 #------------------------------------
-sudo apt-get insntall -y flatpak gnome-software-plugin-flatpak
+sudo apt-get install -y flatpak gnome-software-plugin-flatpak
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 #------------------------------------

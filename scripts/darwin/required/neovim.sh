74a958712ec7679052c154ea08fd62bb294ce17e
2lu3 <hi2lu3@gmail.com>
2025-03-29 10:22:38 +0900

    色々追加

--- scripts/darwin/required/neovim.sh
+++ scripts/darwin/required/neovim.sh
@@ -7,12 +7,13 @@ install fd fdfind
 install trach-cli trash
 install deno deno
 install fzf fzf
+install ripgrep rg
 
 sudo npm install --location=global neovim
 
 if [[ ! -e "$HOME/.pynvim" ]]; then
 python3 -m venv $HOME/.pynvim
-~/.pynvim/bin/pip install -u pip
+~/.pynvim/bin/pip install -U pip
 ~/.pynvim/bin/pip install pynvim doq
 fi
 

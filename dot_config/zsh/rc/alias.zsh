540ab7a89f8074fbaa58cc7b5e3d84dd30409e4a
2lu3 <hi2lu3@gmail.com>
2026-01-05 15:07:31 +0900

    mac用に色々編集した

--- dot_config/zsh/rc/alias.zsh
+++ dot_config/zsh/rc/alias.zsh
@@ -18,18 +18,7 @@ fi
 
 alias pycreate='python3 -m venv .venv && echo "source .venv/bin/activate\nunset PS1" >> .envrc && direnv allow && .venv/bin/pip install -U pip'
 
-alias remoup='systemctl --user start remotessh.service'
-alias remodown='systemctl --user stop remotessh.service'
 alias rds='sudo du -m / | sort -n | tail -10'
-
-if type xsel > /dev/null 2>&1; then
-    alias copy='xsel --clipboard --input'
-fi
-
-
-if type flatpak > /dev/null 2>&1; then
-    alias wezterm='flatpak run org.wezfurlong.wezterm'
-fi
 
 if type oco > /dev/null 2>&1; then
     alias auto='git add -A && oco --yes && git push'

4e6e4047f17c5914c73b2f4ddaf567e5f27251d9
2lu3 <hi2lu3@gmail.com>
2026-02-03 12:46:28 +0900

    ♻️ リファクタ(dot_config/wezterm/wezterm.lua): require文のスタイルを統一し、フォント設定を更新

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,19 +1,17 @@
-require 'format'
-require 'status'
-require 'event'
+require("format")
+require("status")
+require("event")
 
 return {
-    keys = require("keybinds").keys,
-    key_tables = require("keybinds").key_tables,
-    leader = { key = "a", mods = "CTRL" },
-    --disable_default_key_bindings = true,
-    color_scheme = 'MaterialDesignColors',
-    window_background_opacity = 1,
-    font = require("wezterm").font_with_fallback(
-        {"Moralerspace Neon JPDOC"}
-    ),
-    font_size = 11.0,
-    use_ime = true,
-    ime_preedit_rendering = "Builtin",
-    status_update_interval = 1000,
+	keys = require("keybinds").keys,
+	key_tables = require("keybinds").key_tables,
+	leader = { key = "a", mods = "CTRL" },
+	--disable_default_key_bindings = true,
+	color_scheme = "MaterialDesignColors",
+	window_background_opacity = 1,
+	font = require("wezterm").font_with_fallback({ "FiraCode Nerd Font", "Moralerspace Neon JPDOC" }),
+	font_size = 11.0,
+	use_ime = true,
+	ime_preedit_rendering = "Builtin",
+	status_update_interval = 1000,
 }

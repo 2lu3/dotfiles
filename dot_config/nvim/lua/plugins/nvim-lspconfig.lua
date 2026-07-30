1fc37fbe42c600f3e085a51a20e44e9d53b8080e
2lu3 <hi2lu3@gmail.com>
2026-07-30 10:05:25 +0900

    ✨ 新機能(nvim-lspconfig.lua): 診断情報を表示するためのキー割り当てを追加

--- dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -24,6 +24,7 @@ return {
     --     vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
          vim.keymap.set("n", "<leader>ac", vim.lsp.buf.code_action, opts)
+         vim.keymap.set("n", "<leader>if", vim.diagnostic.open_float, opts)
        end,
      })
   end,
